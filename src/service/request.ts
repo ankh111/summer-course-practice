01037c7944eabaa654592be1d2a7e33cf21241a0
hongfaqiu <951142905@qq.com>
2023-08-14 14:39:18 +0800

    chore: replace cors api

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,4 +1,4 @@
-const corsAnywhereUrl = 'https://cors.bridged.cc/';
+const corsAnywhereUrl = 'https://cors.951142905.workers.dev/';
 
 export function request(input: string, init?: RequestInit | undefined): Promise<Response> {
   return fetch(corsAnywhereUrl + input, init);
